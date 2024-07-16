f586b45dfc6bf0771fd993eca0b7e3006b8a8d46
Roldezir <140123210+Roldezir@users.noreply.github.com>
2024-07-16 17:50:57 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let whatsappLogo=document.querySelector(".whatsappLogo")
+function mudarWppLogo(){whatsappLogo.src='whatsappLogo1.png'}
+function voltarWppLogo(){whatsappLogo.src='whatsappLogo.png'}
+
+let redesLogo1=document.querySelector(".imgRedes1")
+function mudarWppLogo1(){redesLogo1.src='instagramLogo1.png'}
+function voltarWppLogo1(){redesLogo1.src='instagramLogo.png'}
+
+let redesLogo2=document.querySelector(".imgRedes2")
+function mudarWppLogo2(){redesLogo2.src='whatsappLogo1.png'}
+function voltarWppLogo2(){redesLogo2.src='whatsappLogo.png'}
+
+let redesLogo3=document.querySelector(".imgRedes3")
+function mudarWppLogo3(){redesLogo3.src='facebookLogo1.png'}
+function voltarWppLogo3(){redesLogo3.src='facebookLogo.png'}
+
+let redesLogo4=document.querySelector(".imgRedes4")
+function mudarWppLogo4(){redesLogo4.src='linkedinLogo1.png'}
+function voltarWppLogo4(){redesLogo4.src='linkedinLogo.png'}
+
---
+++ revelar.js
@@ -0,0 +1,48 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.hidden',{
+    duration: 500,
+    distance: '20px'
+})
+revelar.reveal('.hidden1',{
+    duration: 1000,
+    distance: '20px',
+    delay: 500
+})
+revelar.reveal('.hidden2',{
+    duration: 1000,
+    distance: '20px',
+    delay: 1000
+})
+revelar.reveal('.hidden3',{
+    duration: 1000,
+    distance: '20px',
+    delay: 1500
+})
+revelar.reveal('.hidden4',{
+    duration: 1000,
+    distance: '20px',
+    delay: 500,
+    origin: 'left'
+})
+revelar.reveal('.hidden5',{
+    duration: 1000,
+    distance: '20px',
+    delay: 2000
+})
+revelar.reveal('.hidden6',{
+    duration: 1000,
+    distance: '20px',
+    delay: 2500
+})
+revelar.reveal('.hidden7',{
+    duration: 1000,
+    distance: '20px',
+    delay: 3000
+})
+revelar.reveal('.hidden8',{
+    duration: 1000,
+    distance: '20px',
+    delay: 1000,
+    origin:'right'
+})
